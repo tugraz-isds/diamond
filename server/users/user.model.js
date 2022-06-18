b9aa3ae820eb1efc62ff4326cda687bc38eb2cd6
MeeedAmine <amine.meed@hotmail.fr>
2022-06-18 03:01:05 +0200

    db models updated: schema names

--- server/users/user.model.js
+++ server/users/user.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
+const accountSchema = new Schema({
     email: { type: String, unique: true, required: true },
     hash: { type: String, required: true },
     study: { type: Boolean, unique: false, required: false },
     createdDate: { type: Date, default: Date.now }
 });
 
-schema.set('toJSON', { virtuals: true });
+accountSchema.set('toJSON', { virtuals: true });
 
-module.exports = mongoose.model('User', schema);
+module.exports = mongoose.model('account', accountSchema);
